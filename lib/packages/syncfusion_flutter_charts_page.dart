ed64ff2dd9e07e575b4e1983edf0e13fb71d23ff
Thomas Mora <thomasmoranqn@gmail.com>
2024-05-17 10:50:58 -0300

    mas a sf

--- lib/packages/syncfusion_flutter_charts_page.dart
+++ lib/packages/syncfusion_flutter_charts_page.dart
@@ -3,54 +3,138 @@ import 'package:qr_animations/base_page.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
-class _SalesData {
-  const _SalesData(this.year, this.sales);
+class SalesData {
+  const SalesData(this.year, this.sales);
 
   final String year;
   final double sales;
 }
 
+const List<SalesData> _kData = [
+  SalesData('Jan', 35),
+  SalesData('Feb', 28),
+  SalesData('Mar', 34),
+  SalesData('Apr', 32),
+  SalesData('May', 40)
+];
+
 class SyncfusionFlutterChartsPage extends StatelessWidget {
   const SyncfusionFlutterChartsPage({super.key});
-  static const List<_SalesData> data = [_SalesData('Jan', 35), _SalesData('Feb', 28), _SalesData('Mar', 34), _SalesData('Apr', 32), _SalesData('May', 40)];
 
   @override
   Widget build(BuildContext context) {
-    return BasePage(
+    return const BasePage(
       package: "syncfusion_flutter_charts",
       widgets: [
-        SfCartesianChart(
-            primaryXAxis: CategoryAxis(),
-            // Chart title
-            title: ChartTitle(text: 'Half yearly sales analysis'),
-            // Enable legend
-            legend: Legend(isVisible: true),
-            // Enable tooltip
-            tooltipBehavior: TooltipBehavior(enable: true),
-            series: <CartesianSeries<_SalesData, String>>[
-              LineSeries<_SalesData, String>(
-                  dataSource: data,
-                  xValueMapper: (_SalesData sales, _) => sales.year,
-                  yValueMapper: (_SalesData sales, _) => sales.sales,
-                  name: 'Sales',
-                  // Enable data label
-                  dataLabelSettings: DataLabelSettings(isVisible: true))
-            ]),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          //Initialize the spark charts widget
-          child: SfSparkLineChart.custom(
-            //Enable the trackball
-            trackball: SparkChartTrackball(activationMode: SparkChartActivationMode.tap),
-            //Enable marker
-            marker: SparkChartMarker(displayMode: SparkChartMarkerDisplayMode.all),
-            //Enable data label
-            labelDisplayMode: SparkChartLabelDisplayMode.all,
-            xValueMapper: (int index) => data[index].year,
-            yValueMapper: (int index) => data[index].sales,
-            dataCount: 5,
+        CartesianChart(),
+        Chart2(),
+        PyramidChart(),
+        FunnelChart(),
+      ],
+    );
+  }
+}
+
+class CartesianChart extends StatelessWidget {
+  const CartesianChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      primaryXAxis: CategoryAxis(),
+      // Chart title
+      title: ChartTitle(text: 'Propiedades vendidas'),
+      // Enable legend
+      legend: Legend(isVisible: true),
+      // Enable tooltip
+      tooltipBehavior: TooltipBehavior(enable: true),
+      series: <CartesianSeries<SalesData, String>>[
+        LineSeries<SalesData, String>(
+            dataSource: _kData,
+            xValueMapper: (SalesData sales, _) => sales.year,
+            yValueMapper: (SalesData sales, _) => sales.sales,
+            name: 'Ventas',
+            // Enable data label
+            dataLabelSettings: DataLabelSettings(isVisible: true))
+      ],
+    );
+  }
+}
+
+class Chart2 extends StatelessWidget {
+  const Chart2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      //Initialize the spark charts widget
+      child: SfSparkLineChart.custom(
+        //Enable the trackball
+        trackball: SparkChartTrackball(activationMode: SparkChartActivationMode.tap),
+        //Enable marker
+        marker: SparkChartMarker(displayMode: SparkChartMarkerDisplayMode.all),
+        //Enable data label
+        labelDisplayMode: SparkChartLabelDisplayMode.all,
+        xValueMapper: (int index) => _kData[index].year,
+        yValueMapper: (int index) => _kData[index].sales,
+        dataCount: 5,
+      ),
+    );
+  }
+}
+
+class PyramidChart extends StatelessWidget {
+  const PyramidChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SfPyramidChart(
+      title: ChartTitle(text: 'Piramide'),
+      // Enable legend
+      legend: Legend(isVisible: true),
+
+      // Enable tooltip
+      tooltipBehavior: TooltipBehavior(enable: true),
+      series: PyramidSeries<SalesData, String>(
+        dataSource: _kData,
+        xValueMapper: (SalesData sales, _) => sales.year,
+        yValueMapper: (SalesData sales, _) => sales.sales,
+        name: 'Ventas',
+        // Enable data label
+        dataLabelSettings: DataLabelSettings(isVisible: true),
+      ),
+    );
+  }
+}
+
+class FunnelChart extends StatelessWidget {
+  const FunnelChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SfFunnelChart(
+          title: ChartTitle(text: 'Funnel - Embudo'),
+          // Enable legend
+          legend: Legend(isVisible: true),
+
+          // Enable tooltip
+          tooltipBehavior: TooltipBehavior(enable: true),
+          series: FunnelSeries<SalesData, String>(
+            dataSource: _kData,
+            xValueMapper: (SalesData sales, _) => sales.year,
+            yValueMapper: (SalesData sales, _) => sales.sales,
+            name: 'Ventas',
+            gapRatio: 0.1,
+            neckWidth: "15%",
+            neckHeight: "0%",
+            // Enable data label
+            dataLabelSettings: DataLabelSettings(isVisible: true),
           ),
         ),
+        
       ],
     );
   }
--- lib/packages/wave_linear_progress_indicator_page.dart
+++ lib/packages/wave_linear_progress_indicator_page.dart
@@ -2,7 +2,268 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:qr_animations/base_page.dart';
-import 'package:wave_linear_progress_indicator/wave_linear_progress_indicator.dart';
+
+class WaveIndicatorPainter extends CustomPainter {
+  WaveIndicatorPainter({
+    required this.repaint,
+    required this.progressAnimation,
+    required this.backgroundColor,
+    required this.borderRadius,
+    required this.waveWidth,
+    required this.waveBackgroundColor,
+    required this.waveStep,
+    required this.waveColor,
+  }) : super(repaint: Listenable.merge([repaint, progressAnimation]));
+
+  final Animation<double> repaint;
+  final Animation<double> progressAnimation;
+  final Color backgroundColor;
+  final double? borderRadius;
+  final double waveWidth;
+  final Color waveBackgroundColor;
+  final double waveStep;
+  final Color waveColor;
+
+  final Paint _paint = Paint();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // 剪裁画布
+    if (borderRadius != null && borderRadius! > 0) {
+      canvas.clipRRect(RRect.fromRectAndRadius(Offset.zero & size, Radius.circular(borderRadius!)));
+    } else {
+      canvas.clipRect(Offset.zero & size);
+    }
+
+    _drawWaves(canvas, size);
+
+    // 把背景盖在波纹上
+    _drawBG(canvas, size);
+  }
+
+  void _drawBG(Canvas canvas, Size size) {
+    final progress = progressAnimation.value;
+    final left = size.width * progress;
+    _paint
+      ..color = backgroundColor
+      ..style = PaintingStyle.fill;
+    final rect = Offset(left, 0) & Size(size.width - left, size.height);
+    canvas.drawRect(rect, _paint);
+  }
+
+  void _drawWaves(Canvas canvas, Size size) {
+    canvas.save();
+    // final progress = progressAnimation.value;
+    // final width = size.width * progress;
+    _paint
+      ..color = waveBackgroundColor
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset.zero & size, _paint);
+    canvas.clipRect(Offset.zero & size);
+
+    const angle = 20;
+    _paint
+      ..color = waveColor
+      ..style = PaintingStyle.fill;
+    final count = (size.width / (waveStep + waveWidth)).ceil();
+
+    final height = size.height * 2.0;
+
+    final realWidth = (waveWidth + waveStep) * count;
+    final offset = -realWidth * repaint.value;
+    canvas.translate(offset, 0);
+
+    for (int i = 0; i < count * 2; i++) {
+      var dx = (waveWidth + waveStep) * i;
+
+      canvas.save();
+      canvas.translate(dx + waveWidth / 2, 0);
+      canvas.rotate(angle * pi / 180);
+
+      canvas.drawRect(Offset(0, -(height - size.height) / 2) & Size(waveWidth, height), _paint);
+
+      canvas.restore();
+    }
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(covariant WaveIndicatorPainter oldDelegate) =>
+      oldDelegate.backgroundColor != backgroundColor ||
+      oldDelegate.repaint != repaint ||
+      oldDelegate.progressAnimation != progressAnimation ||
+      oldDelegate.borderRadius != borderRadius ||
+      oldDelegate.waveWidth != waveWidth ||
+      oldDelegate.waveStep != waveStep ||
+      oldDelegate.waveBackgroundColor != waveBackgroundColor ||
+      oldDelegate.waveColor != waveColor;
+}
+
+class WaveLinearProgressIndicator extends ProgressIndicator {
+  const WaveLinearProgressIndicator({
+    super.key,
+    required super.value,
+    super.backgroundColor = const Color(0xFFECF4F2),
+    super.color,
+    this.minHeight,
+    this.borderRadius = 18,
+    this.waveWidth = 10,
+    this.waveColor = const Color(0x21FFFFFF),
+    this.waveBackgroundColor = const Color(0xFF71E4D6),
+    this.waveStep = 8,
+    this.labelDecoration,
+    this.enableBounceAnimation = false,
+  });
+
+  /// The minimum height of the line used to draw the linear indicator.
+  final double? minHeight;
+
+  /// Rounded corners of the progress indicator
+  final double? borderRadius;
+
+  /// Width of wave
+  final double waveWidth;
+
+  /// Background color of the progress indicator
+  final Color waveBackgroundColor;
+
+  /// Spacing from wave to the next wave
+  final double waveStep;
+
+  /// Color of the spacing between waves
+  final Color waveColor;
+
+  /// Decoration of the progress label widget
+  final Decoration? labelDecoration;
+
+  /// Whether to turn on the bouncing animation effect
+  final bool enableBounceAnimation;
+
+  @override
+  State<StatefulWidget> createState() => _WaveLinearProgressIndicatorState();
+}
+
+class _WaveLinearProgressIndicatorState extends State<WaveLinearProgressIndicator> with TickerProviderStateMixin {
+  late AnimationController _waveController;
+  late AnimationController _progressController;
+  late Animation<double> _progressAnimation;
+
+  final GlobalKey _progressLabelKey = GlobalKey();
+
+  Decoration get _labelDecoration {
+    if (widget.labelDecoration != null) {
+      return widget.labelDecoration!;
+    }
+    return BoxDecoration(
+      gradient: const LinearGradient(
+        colors: [
+          Color(0xFF9DF3E9),
+          Color(0xFF71E4D6),
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      ),
+      border: Border.all(
+        color: Colors.white,
+        width: 1,
+      ),
+      borderRadius: BorderRadius.circular(13),
+    );
+  }
+
+  Color get _backgroundColor {
+    final Color trackColor =
+        widget.backgroundColor ?? _indicatorTheme.linearTrackColor ?? Theme.of(context).colorScheme.background;
+    return trackColor;
+  }
+
+  double get _minHeight => widget.minHeight ?? _indicatorTheme.linearMinHeight ?? 9;
+
+  double get _progressLabelHeight => _minHeight + 4.5 * 2;
+
+  ProgressIndicatorThemeData get _indicatorTheme => ProgressIndicatorTheme.of(context);
+
+  @override
+  void initState() {
+    _waveController = AnimationController(vsync: this, duration: const Duration(milliseconds: 5000));
+    _waveController.repeat();
+    _progressController = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+
+    _progressAnimation = Tween(begin: widget.value).animate(_progressController);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant WaveLinearProgressIndicator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final oldValue = oldWidget.value ?? 0;
+    final newValue = widget.value ?? 0;
+    if (widget.enableBounceAnimation) {
+      double bouncingValue = newValue + (10 / 100) * ((newValue - oldValue) / (newValue - oldValue).abs());
+      bouncingValue = bouncingValue.clamp(0, 1.0).toDouble();
+      _progressAnimation = TweenSequence(<TweenSequenceItem<double>>[
+        TweenSequenceItem(
+          tween: Tween(begin: oldValue, end: bouncingValue).chain(CurveTween(curve: Curves.ease)),
+          weight: 3 / 4,
+        ),
+        TweenSequenceItem(
+          tween: Tween(begin: bouncingValue, end: newValue).chain(CurveTween(curve: Curves.ease)),
+          weight: 1 / 4,
+        ),
+      ]).animate(_progressController);
+    } else {
+      _progressAnimation = Tween(begin: oldValue, end: newValue).animate(_progressController);
+    }
+    _progressController.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    _waveController.dispose();
+    _progressController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minWidth: double.infinity,
+        minHeight: _progressLabelHeight,
+      ),
+      child: LayoutBuilder(builder: (context, constraints) {
+        // print('=======layout===$constraints');
+        return Stack(
+          alignment: Alignment.center,
+          clipBehavior: Clip.none,
+          children: [
+            Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: _minHeight,
+                  minWidth: double.infinity,
+                ),
+                child: CustomPaint(
+                  painter: WaveIndicatorPainter(
+                    repaint: _waveController,
+                    progressAnimation: _progressAnimation,
+                    backgroundColor: _backgroundColor,
+                    borderRadius: widget.borderRadius,
+                    waveWidth: widget.waveWidth,
+                    waveBackgroundColor: widget.waveBackgroundColor,
+                    waveStep: widget.waveStep,
+                    waveColor: widget.waveColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
 
 class WaveLinearProgressIndicatorPage extends StatefulWidget {
   const WaveLinearProgressIndicatorPage({super.key});
